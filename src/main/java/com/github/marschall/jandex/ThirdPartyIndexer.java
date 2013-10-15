272a7b095aee00152dbf417d16d6e3e3fed42203
Philippe Marschall <pm@netcetera.ch>
2013-10-15 09:25:20 +0200

    indentation

--- src/main/java/com/github/marschall/jandex/ThirdPartyIndexer.java
+++ src/main/java/com/github/marschall/jandex/ThirdPartyIndexer.java
@@ -50,102 +50,102 @@ import org.jboss.jandex.Indexer;
  * @see http://maven.apache.org/plugin-tools/maven-plugin-plugin/examples/using-annotations.html
  */
 @Mojo(name = "index",
-	threadSafe = true,
-	defaultPhase = LifecyclePhase.PREPARE_PACKAGE,
-	requiresDependencyCollection = ResolutionScope.COMPILE_PLUS_RUNTIME)
+  threadSafe = true,
+  defaultPhase = LifecyclePhase.PREPARE_PACKAGE,
+  requiresDependencyCollection = ResolutionScope.COMPILE_PLUS_RUNTIME)
 @Execute(goal = "index",
-	phase = LifecyclePhase.PREPARE_PACKAGE)
+  phase = LifecyclePhase.PREPARE_PACKAGE)
 public class ThirdPartyIndexer extends AbstractMojo {
-	
-    @Component
-    private MavenProject project;
-	
-	
-	/**
-	 * The folder that contains the JARs which should be indexed.
-	 */
-	@Parameter(defaultValue = "${project.build.directory}/${project.build.finalName}/lib")
-	private File archiveFolder;
 
-	@Override
-	public void execute() throws MojoExecutionException, MojoFailureException {
-		Path archtiveFolderPath = this.archiveFolder.toPath();
-		if (!Files.exists(archtiveFolderPath)) {
-			try {
-				Files.createDirectories(archtiveFolderPath);
-			} catch (IOException e) {
-				throw new MojoExecutionException("could not create archive folder " + this.archiveFolder, e);
-			}
-		}
-		try {
-			indexDependencies();
-		} catch (IOException e) {
-			throw new MojoExecutionException("could not create indices", e);
-		}
-	}
-	
-	
-	private void indexDependencies() throws IOException {
-		for (Artifact artifact : this.project.getArtifacts()) {
-			String type = artifact.getType();
-			if ("jar".equals(type)) {
-				String scope = artifact.getScope();
-				if (SCOPE_COMPILE.equals(scope) || SCOPE_RUNTIME.equals(scope)) {
-					Path artifactPath = artifact.getFile().toPath();
-					createIndex(artifactPath);
-				}
-			}
-		}
-	}
-	
-	private void createIndex(Path jar) throws IOException {
-		Index index = index(jar);
-		if (index == null) {
-			return;
-		}
-		
-		// REVIEW may fail for SNAPSHOT dependencies
-		Path indexFile = this.archiveFolder.toPath().resolve(jar.getFileName().toString() + ".index");
-		try (OutputStream outputStream = Files.newOutputStream(indexFile, WRITE, CREATE)) {
-			IndexWriter indexWriter = new IndexWriter(outputStream);
-			// IndexWriter does buffering
-			indexWriter.write(index);
-		}
-	}
-	
-	private Index index(Path jar) throws IOException {
-		Map<String, String> env = new HashMap<>(1); 
-        env.put("create", "false");
-        // locate file system by using the syntax 
-        // defined in java.net.JarURLConnection
-        URI uri = URI.create("jar:" + jar.toUri());
-        try (FileSystem zipfs = FileSystems.newFileSystem(uri, env)) {
-        	Path root = zipfs.getPath("/");
-        	Path jandexIdx = root.resolve("META-INF/jandex.idx");
-        	if (Files.exists(jandexIdx)) {
-				return null;
-			}
-        	
-        	final Indexer indexer = new Indexer();
-			Files.walkFileTree(root, new SimpleFileVisitor<Path>() {
+  @Component
+  private MavenProject project;
 
 
-				@Override
-				public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-					if (attrs.isRegularFile()) {
-						boolean isClass = file.getFileName().toString().endsWith(".class");
-						if (isClass) {
-							try (InputStream inputStream = Files.newInputStream(file, READ)) {
-								// indexer does buffering
-								indexer.index(inputStream);
-							}
-						}
-					}
-					return FileVisitResult.CONTINUE;
-				}
-			});
-        	return indexer.complete();
+  /**
+   * The folder that contains the JARs which should be indexed.
+   */
+  @Parameter(defaultValue = "${project.build.directory}/${project.build.finalName}/lib")
+  private File archiveFolder;
+
+  @Override
+  public void execute() throws MojoExecutionException, MojoFailureException {
+    Path archtiveFolderPath = this.archiveFolder.toPath();
+    if (!Files.exists(archtiveFolderPath)) {
+      try {
+        Files.createDirectories(archtiveFolderPath);
+      } catch (IOException e) {
+        throw new MojoExecutionException("could not create archive folder " + this.archiveFolder, e);
+      }
+    }
+    try {
+      indexDependencies();
+    } catch (IOException e) {
+      throw new MojoExecutionException("could not create indices", e);
+    }
+  }
+
+
+  private void indexDependencies() throws IOException {
+    for (Artifact artifact : this.project.getArtifacts()) {
+      String type = artifact.getType();
+      if ("jar".equals(type)) {
+        String scope = artifact.getScope();
+        if (SCOPE_COMPILE.equals(scope) || SCOPE_RUNTIME.equals(scope)) {
+          Path artifactPath = artifact.getFile().toPath();
+          createIndex(artifactPath);
         }
-	}
+      }
+    }
+  }
+
+  private void createIndex(Path jar) throws IOException {
+    Index index = index(jar);
+    if (index == null) {
+      return;
+    }
+
+    // REVIEW may fail for SNAPSHOT dependencies
+    Path indexFile = this.archiveFolder.toPath().resolve(jar.getFileName().toString() + ".index");
+    try (OutputStream outputStream = Files.newOutputStream(indexFile, WRITE, CREATE)) {
+      IndexWriter indexWriter = new IndexWriter(outputStream);
+      // IndexWriter does buffering
+      indexWriter.write(index);
+    }
+  }
+
+  private Index index(Path jar) throws IOException {
+    Map<String, String> env = new HashMap<>(1); 
+    env.put("create", "false");
+    // locate file system by using the syntax 
+    // defined in java.net.JarURLConnection
+    URI uri = URI.create("jar:" + jar.toUri());
+    try (FileSystem zipfs = FileSystems.newFileSystem(uri, env)) {
+      Path root = zipfs.getPath("/");
+      Path jandexIdx = root.resolve("META-INF/jandex.idx");
+      if (Files.exists(jandexIdx)) {
+        return null;
+      }
+
+      final Indexer indexer = new Indexer();
+      Files.walkFileTree(root, new SimpleFileVisitor<Path>() {
+
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+          if (attrs.isRegularFile()) {
+            boolean isClass = file.getFileName().toString().endsWith(".class");
+            if (isClass) {
+              try (InputStream inputStream = Files.newInputStream(file, READ)) {
+                // indexer does buffering
+                indexer.index(inputStream);
+              }
+            }
+          }
+          return FileVisitResult.CONTINUE;
+        }
+      });
+      return indexer.complete();
+    }
+  }
 
 }
